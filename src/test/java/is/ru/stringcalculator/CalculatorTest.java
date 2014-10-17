13d4ed282224bbcffaa496e139ea6409138995a7
Kolla <kolbrunf87@gmail.com>
2014-10-17 23:27:17 +0000

    búin ad gera main fallid þannig að það uppfylli testið

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
-    }
+      	org.junit.runner.JUnitCore.main("is.ru.stringcalculator.Calculator");
+    	}
 
 	@Test
 	public void testEmptyString() {
@@ -30,6 +30,6 @@ public class CalculatorTest {
 	
 	@Test
 	public void testnewline(){
-		assertEquals(6, Calculator.add("\n2,3");
+		assertEquals(6, Calculator.add("1\n2,3"));
 	}
 }
